b7e6956ab1e12b6b0b7ca0ef9031f9d7e95a4117
Hatimloha <139457449+Hatimloha@users.noreply.github.com>
2024-09-08 03:52:07 -0700

    Update my_table.tf

--- terraform-practice/aws_infra/my_table.tf
+++ terraform-practice/aws_infra/my_table.tf
@@ -1,6 +1,6 @@
 resource "aws_dynamodb_table" "name" {
   name = "${var.my-env}-tws-free-devops-bootcamp-table"
-  billing_mode = "pay_per_request"
+  billing_mode = "PAY_PER_REQUEST"
   hash_key = "id"
   attribute {
     name = "id"
